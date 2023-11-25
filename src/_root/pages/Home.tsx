d52211c528308f4d87f775ffc89cebde40f291b1
hayan2 <uhayan2@gmail.com>
2023-11-25 14:50:30 +0900

    first commit

---
+++ src/_root/pages/Home.tsx
@@ -0,0 +1,42 @@
+import Loader from '@/components/shared/Loader';
+import PostCard from '@/components/shared/PostCard';
+import { usegetRecentPosts } from '@/lib/react-query/queriesAndMutations';
+import { Models } from 'appwrite';
+
+const Home = () => {
+	const { data: posts, isLoading: isPostLoading, isError: isErrorPosts } = usegetRecentPosts();
+
+	if (isErrorPosts) {
+		return (
+			<div className="flex flex-1">
+				<div className="home-container">
+					<p className="body-medium text-light-1">Something bad happened</p>
+				</div>
+				<div className="home-creators">
+					<p className="body-medium text-light-1">Something bad happened</p>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex flex-1">
+			<div className="home-container">
+				<div className="home-posts">
+					<h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+					{isPostLoading && !posts ? (
+						<Loader />
+					) : (
+						<ul className="flex flex-col flex-1 gap-9 w-full">
+							{posts?.documents.map((post: Models.Document) => (
+								<PostCard post={post} key={post.caption} />
+							))}
+						</ul>
+					)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Home
---
+++ src/components/shared/index.ts
@@ -0,0 +1,3 @@
+export { default as GridPostList } from './GridPostList'
+export { default as Loader } from './Loader'
+export { default as UserCard } from './UserCard'
